3edd1fb1bc59844b513db957531e0a0cc73c0354
Kaushik, Arpit <kaushi@deshaw.com>
2023-09-09 19:26:22 +0530

    Added states and APP file.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.states.State;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,82 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Welcome to my vending machine." );
+
+        VendingMachine vendingMachine = new VendingMachine();
+        try {
+
+            System.out.println("|");
+            System.out.println("filling up the inventory");
+            System.out.println("|");
+
+            fillUpInventory(vendingMachine);
+            displayInventory(vendingMachine);
+
+            System.out.println("|");
+            System.out.println("clicking on InsertCoinButton");
+            System.out.println("|");
+
+            State vendingState = vendingMachine.getState();
+            vendingState.pressInsertCoin();
+
+            vendingState = vendingMachine.getState();
+            vendingState.insertCoin(Coin.FIVE_RUPEES);
+            vendingState.insertCoin(Coin.ONE_RUPEES);
+
+            System.out.println("|");
+            System.out.println("clicking on ProductSelectionButton");
+            System.out.println("|");
+            vendingState.pressProductSelectionButton();
+
+            vendingState = vendingMachine.getState();
+            vendingState.selectProduct(102);
+
+
+            vendingState = vendingMachine.getState();
+            vendingState.dispenseProduct(102);
+            displayInventory(vendingMachine);
+        }
+        catch (Exception e){
+            displayInventory(vendingMachine);
+        }
+
+
     }
+
+    private static void fillUpInventory(VendingMachine vendingMachine){
+        ItemShelf[] slots = vendingMachine.getInventory().getItemShelves();
+        for (int i = 0; i < slots.length; i++) {
+            Item newItem = new Item();
+            if(i >=0 && i<3) {
+                newItem.setItemType(ItemType.COKE);
+                newItem.setPrice(3);
+            }else if(i >=3 && i<5){
+                newItem.setItemType(ItemType.PEPSI);
+                newItem.setPrice(9);
+            }else if(i >=5 && i<7){
+                newItem.setItemType(ItemType.JUICE);
+                newItem.setPrice(13);
+            }else if(i >=7 && i<10){
+                newItem.setItemType(ItemType.SODA);
+                newItem.setPrice(7);
+            }
+            slots[i].setItem(newItem);
+            slots[i].setSoldOut(false);
+        }
+    }
+
+    private static void displayInventory(VendingMachine vendingMachine){
+
+        ItemShelf[] slots = vendingMachine.getInventory().getItemShelves();
+        for (int i = 0; i < slots.length; i++) {
+
+            System.out.println("CodeNumber: " + slots[i].getCode() +
+                    " Item: " + slots[i].getItem().getItemType().name() +
+                    " Price: " + slots[i].getItem().getPrice() +
+                    " isAvailable: " + !slots[i].getSoldOut());
+        }
+    }
+
 }
